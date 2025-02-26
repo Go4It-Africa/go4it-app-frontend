4f5b337c5065642279617a042163f6d4b76933a0
Norman Munge <normanmunge@gmail.com>
2025-02-26 03:27:17 +0300

    Removes auth from config middleware checks

--- middleware.ts
+++ middleware.ts
@@ -106,7 +106,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico|images|logos|auth).*)',
-  ],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|images|logos).*)'],
 };
